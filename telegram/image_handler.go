c302fe067d126b65c03ac88eeccf0d4e6b5d63f2
hsiangsun <hsiangsun1997@gmail.com>
2023-02-17 19:43:04 +0800

    下游给上游回复视频和图片

--- telegram/image_handler.go
+++ telegram/image_handler.go
@@ -42,122 +42,191 @@ func chanelOutImgHandler(c tb.Context) error {
 
 //下游操作
 func chanelInImgHandler(c tb.Context, currentChannel model.Channel) error {
-	text := c.Text()
 
-	firstOrderId := GetOrderFromText(text)
-	//为空不处理
-	if firstOrderId == "" {
-		return nil
-	}
+	//下游单独的消息
+	if c.Update().Message.ReplyTo == nil {
+		text := c.Text()
 
-	apiRsp, err := GetPlatByOrderId(firstOrderId)
-	if err != nil {
-		log.Sugar.Errorf("chanelInImgHandler api get error:%s", err.Error())
-		return err
-	}
-
-	channelId := apiRsp.Data.ChannelId
-
-	channel, err := FindChanelByPlatId(channelId)
-	if err != nil {
-
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return c.Reply("当前单号尚未绑定上游,请先绑定上游")
+		firstOrderId := GetOrderFromText(text)
+		//为空不处理
+		if firstOrderId == "" {
+			return nil
 		}
 
-		log.Sugar.Errorf("FindChanelByPlatId has error:%s", err.Error())
-		return err
-	}
+		apiRsp, err := GetPlatByOrderId(firstOrderId)
+		if err != nil {
+			log.Sugar.Errorf("chanelInImgHandler api get error:%s", err.Error())
+			return err
+		}
 
-	//--------------是订单内容并且有图片---------------------
+		channelId := apiRsp.Data.ChannelId
 
-	//var newPhoto model.Photo
+		channel, err := FindChanelByPlatId(channelId)
+		if err != nil {
 
-	//如果没有图片就不处理
-	if c.Update().Message.Photo == nil {
-		return nil
-	}
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				return c.Reply("当前单号尚未绑定上游,请先绑定上游")
+			}
 
-	originMessage := c.Message()
-	//fmt.Printf("originMessage:%+v \n", *originMessage)
+			log.Sugar.Errorf("FindChanelByPlatId has error:%s", err.Error())
+			return err
+		}
 
-	originMessageBytes, jerr := json.Marshal(*originMessage)
-	if jerr != nil {
-		log.Sugar.Errorf("josn 序列化原始消息错误:%s", jerr.Error())
-		return nil
-	}
+		//--------------是订单内容并且有图片---------------------
 
-	//保存原始信息到数据库
+		//var newPhoto model.Photo
 
-	uniqueID := c.Update().Message.Photo.File.UniqueID
+		//如果没有图片就不处理
+		if c.Update().Message.Photo == nil {
+			return nil
+		}
 
-	newPhoto := model.Photo{UniqueID: uniqueID, InMessage: string(originMessageBytes)}
+		originMessage := c.Message()
+		//fmt.Printf("originMessage:%+v \n", *originMessage)
 
-	err = orm.Gdb.Model(model.Photo{}).Create(&newPhoto).Error
-	if err != nil {
-		if strings.Contains(err.Error(), "UNIQUE constraint") {
+		originMessageBytes, jerr := json.Marshal(*originMessage)
+		if jerr != nil {
+			log.Sugar.Errorf("josn 序列化原始消息错误:%s", jerr.Error())
+			return nil
+		}
 
-			//加急
-			var outChanPhoto model.Photo
-			//拿到原始上游群信息
-			err = orm.Gdb.Model(model.Photo{}).First(&outChanPhoto).Where("unique_id = ?", uniqueID).Error
+		//保存原始信息到数据库
+
+		uniqueID := c.Update().Message.Photo.File.UniqueID
+
+		newPhoto := model.Photo{UniqueID: uniqueID, InMessage: string(originMessageBytes)}
+
+		err = orm.Gdb.Model(model.Photo{}).Create(&newPhoto).Error
+		if err != nil {
+			if strings.Contains(err.Error(), "UNIQUE constraint") {
+
+				//加急
+				var outChanPhoto model.Photo
+				//拿到原始上游群信息
+				err = orm.Gdb.Model(model.Photo{}).First(&outChanPhoto).Where("unique_id = ?", uniqueID).Error
+				if err != nil {
+					log.Sugar.Errorf("查询上游群图片信息失败:%s", err.Error())
+					return err
+				}
+
+				//转发到上游群
+				to := tb.Chat{ID: channel.ChannelId}
+
+				var outChanMsg tb.Message
+
+				jerr = json.Unmarshal([]byte(outChanPhoto.OutMessage), &outChanMsg)
+				if jerr != nil {
+					log.Sugar.Errorf("json反序列化上游群消息响应失败:%s", err.Error())
+				}
+
+				sendOpts := tb.SendOptions{}
+				sendOpts.ReplyTo = &outChanMsg
+
+				//_, err = c.Bot().Forward(&to, c.Message(), tb.ForceReply)
+				_, err = c.Bot().Send(&to, "这笔单子加急!麻烦尽快处理一下!", &sendOpts)
+				if err != nil {
+					log.Sugar.Errorf("Forward error:%s", err.Error())
+					return err
+				}
+
+				return c.Reply("已加急~ 请勿重复发送图片哦")
+			}
+
+			log.Sugar.Errorf("insert data to photo has error:%s", err.Error())
+
+			return err
+		}
+
+		//开始转发操作
+		to := tb.Chat{ID: channel.ChannelId}
+
+		photo := c.Message().Photo
+		photo.Caption = c.Text()
+
+		outMessage, err := c.Bot().Send(&to, photo) //tb.Protected
+		if err != nil {
+			log.Sugar.Errorf("Forward error:%s", err.Error())
+			return err
+		}
+
+		originOutMessageBytes, jerr := json.Marshal(*outMessage)
+		if jerr != nil {
+			log.Sugar.Errorf("josn 序列化原始响应消息错误:%s", jerr.Error())
+			return nil
+		}
+
+		//保存上游信息
+		err = orm.Gdb.Model(model.Photo{}).Where("id = ?", newPhoto.ID).Update("out_message", string(originOutMessageBytes)).Error
+		if err != nil {
+			log.Sugar.Errorf("update data to photo has error:%s", err.Error())
+			return err
+		}
+
+		return c.Reply("正在为你处理中... 请稍等~")
+	} else { //下游回复给上游的消息
+		if c.Update().Message.Photo == nil {
+			return nil
+		}
+
+		var uniqueID string
+		if c.Message().ReplyTo.Photo != nil { //回复的是图片信息
+			uniqueID = c.Message().ReplyTo.Photo.UniqueID
+		} else if c.Message().ReplyTo.Video != nil {
+			uniqueID = c.Message().ReplyTo.Video.UniqueID
+		}
+
+		var dbPhoto model.Photo
+		err := orm.Gdb.Model(model.Photo{}).Where("unique_id = ?", uniqueID).First(&dbPhoto).Error
+		if err != nil {
+			log.Sugar.Errorf("sql query from photo has error:%s", err.Error())
+			return nil
+		}
+
+		// 图片是否来自下游群
+		if dbPhoto.ID > 0 {
+
+			//反序列化原始消息
+			originMessageStr := dbPhoto.OutMessage
+
+			var message tb.Message
+
+			err = json.Unmarshal([]byte(originMessageStr), &message)
 			if err != nil {
-				log.Sugar.Errorf("查询上游群图片信息失败:%s", err.Error())
+				log.Sugar.Errorf("反序列化原始消息错误:%s", err.Error())
 				return err
 			}
 
-			//转发到上游群
-			to := tb.Chat{ID: channel.ChannelId}
-
-			var outChanMsg tb.Message
-
-			jerr = json.Unmarshal([]byte(outChanPhoto.OutMessage), &outChanMsg)
-			if jerr != nil {
-				log.Sugar.Errorf("json反序列化上游群消息响应失败:%s", err.Error())
-			}
+			//转发到下游群
+			to := tb.Chat{ID: message.Chat.ID}
 
 			sendOpts := tb.SendOptions{}
-			sendOpts.ReplyTo = &outChanMsg
+			sendOpts.ReplyTo = &message
 
-			//_, err = c.Bot().Forward(&to, c.Message(), tb.ForceReply)
-			_, err = c.Bot().Send(&to, "这笔单子加急!麻烦尽快处理一下!", &sendOpts)
+			//回复给下游的消息
+			var replayMsg interface{}
+			//上游回复是否带图片
+			if c.Update().Message.Photo != nil {
+				photo := c.Message().Photo
+				photo.Caption = c.Text()
+				replayMsg = photo
+			} else if c.Update().Message.Video != nil {
+				video := c.Message().Video
+				video.Caption = c.Text()
+				replayMsg = video
+			} else {
+				replayMsg = c.Message().Text
+			}
+
+			_, err = c.Bot().Send(&to, replayMsg, &sendOpts)
 			if err != nil {
 				log.Sugar.Errorf("Forward error:%s", err.Error())
 				return err
 			}
 
-			return c.Reply("已加急~ 请勿重复发送图片哦")
 		}
 
-		log.Sugar.Errorf("insert data to photo has error:%s", err.Error())
+		return c.Reply("正在为你处理中... 请稍等~")
 
-		return err
 	}
 
-	//开始转发操作
-	to := tb.Chat{ID: channel.ChannelId}
-
-	photo := c.Message().Photo
-	photo.Caption = c.Text()
-
-	outMessage, err := c.Bot().Send(&to, photo) //tb.Protected
-	if err != nil {
-		log.Sugar.Errorf("Forward error:%s", err.Error())
-		return err
-	}
-
-	originOutMessageBytes, jerr := json.Marshal(*outMessage)
-	if jerr != nil {
-		log.Sugar.Errorf("josn 序列化原始响应消息错误:%s", jerr.Error())
-		return nil
-	}
-
-	//保存上游信息
-	err = orm.Gdb.Model(model.Photo{}).Where("id = ?", newPhoto.ID).Update("out_message", string(originOutMessageBytes)).Error
-	if err != nil {
-		log.Sugar.Errorf("update data to photo has error:%s", err.Error())
-		return err
-	}
-
-	return c.Reply("正在为你处理中... 请稍等~")
 }
--- telegram/video_handler.go
+++ telegram/video_handler.go
@@ -42,119 +42,182 @@ func chanelOutVideoHandler(c tb.Context) error {
 
 //下游操作
 func chanelInVideoHandler(c tb.Context, currentChanel model.Channel) error {
-	text := c.Text()
-	firstOrderId := GetOrderFromText(text)
+	if c.Update().Message.ReplyTo == nil {
+		text := c.Text()
+		firstOrderId := GetOrderFromText(text)
 
-	apiRsp, err := GetPlatByOrderId(firstOrderId)
-	if err != nil {
-		log.Sugar.Errorf("chanelInImgHandler api get error:%s", err.Error())
-		return err
-	}
-
-	channelId := apiRsp.Data.ChannelId
-
-	channel, err := FindChanelByPlatId(channelId)
-	if err != nil {
-
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return c.Reply("当前单号尚未绑定上游,请先绑定上游")
+		apiRsp, err := GetPlatByOrderId(firstOrderId)
+		if err != nil {
+			log.Sugar.Errorf("chanelInImgHandler api get error:%s", err.Error())
+			return err
 		}
 
-		log.Sugar.Errorf("FindChanelByPlatId has error:%s", err.Error())
-		return err
-	}
+		channelId := apiRsp.Data.ChannelId
 
-	//--------------是订单内容并且有图片---------------------
+		channel, err := FindChanelByPlatId(channelId)
+		if err != nil {
 
-	//var newPhoto model.Photo
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				return c.Reply("当前单号尚未绑定上游,请先绑定上游")
+			}
 
-	//如果没有视频就不处理
-	if c.Update().Message.Video == nil {
-		return nil
-	}
+			log.Sugar.Errorf("FindChanelByPlatId has error:%s", err.Error())
+			return err
+		}
 
-	originMessage := c.Message()
-	//fmt.Printf("originMessage:%+v \n", *originMessage)
+		//--------------是订单内容并且有图片---------------------
 
-	originMessageBytes, jerr := json.Marshal(*originMessage)
-	if jerr != nil {
-		log.Sugar.Errorf("josn 序列化原始消息错误:%s", jerr.Error())
-		return nil
-	}
+		//var newPhoto model.Photo
 
-	//保存原始信息到数据库
+		//如果没有视频就不处理
+		if c.Update().Message.Video == nil {
+			return nil
+		}
 
-	uniqueID := c.Update().Message.Video.File.UniqueID
+		originMessage := c.Message()
+		//fmt.Printf("originMessage:%+v \n", *originMessage)
 
-	newPhoto := model.Photo{UniqueID: uniqueID, InMessage: string(originMessageBytes)}
+		originMessageBytes, jerr := json.Marshal(*originMessage)
+		if jerr != nil {
+			log.Sugar.Errorf("josn 序列化原始消息错误:%s", jerr.Error())
+			return nil
+		}
 
-	err = orm.Gdb.Model(model.Photo{}).Create(&newPhoto).Error
-	if err != nil {
-		if strings.Contains(err.Error(), "UNIQUE constraint") {
+		//保存原始信息到数据库
 
-			//加急
-			var outChanPhoto model.Photo
-			//拿到原始上游群信息
-			err = orm.Gdb.Model(model.Photo{}).First(&outChanPhoto).Where("unique_id = ?", uniqueID).Error
+		uniqueID := c.Update().Message.Video.File.UniqueID
+
+		newPhoto := model.Photo{UniqueID: uniqueID, InMessage: string(originMessageBytes)}
+
+		err = orm.Gdb.Model(model.Photo{}).Create(&newPhoto).Error
+		if err != nil {
+			if strings.Contains(err.Error(), "UNIQUE constraint") {
+
+				//加急
+				var outChanPhoto model.Photo
+				//拿到原始上游群信息
+				err = orm.Gdb.Model(model.Photo{}).First(&outChanPhoto).Where("unique_id = ?", uniqueID).Error
+				if err != nil {
+					log.Sugar.Errorf("查询上游群图片信息失败:%s", err.Error())
+					return err
+				}
+
+				//转发到上游群
+				to := tb.Chat{ID: channel.ChannelId}
+
+				var outChanMsg tb.Message
+
+				jerr = json.Unmarshal([]byte(outChanPhoto.OutMessage), &outChanMsg)
+				if jerr != nil {
+					log.Sugar.Errorf("json反序列化上游群消息响应失败:%s", err.Error())
+				}
+
+				sendOpts := tb.SendOptions{}
+				sendOpts.ReplyTo = &outChanMsg
+
+				//_, err = c.Bot().Forward(&to, c.Message(), tb.ForceReply)
+				_, err = c.Bot().Send(&to, "这笔单子加急!麻烦尽快处理一下!", &sendOpts)
+				if err != nil {
+					log.Sugar.Errorf("Forward error:%s", err.Error())
+					return err
+				}
+
+				return c.Reply("已加急~ 请勿重复发送视频哦")
+			}
+
+			log.Sugar.Errorf("insert data to photo has error:%s", err.Error())
+
+			return err
+		}
+
+		//开始转发操作
+		to := tb.Chat{ID: channel.ChannelId}
+
+		//photo := c.Message().Photo
+		//photo.Caption = c.Text()
+
+		video := c.Message().Video
+		video.Caption = c.Text()
+
+		outMessage, err := c.Bot().Send(&to, video) //tb.Protected
+		if err != nil {
+			log.Sugar.Errorf("Forward error:%s", err.Error())
+			return err
+		}
+
+		originOutMessageBytes, jerr := json.Marshal(*outMessage)
+		if jerr != nil {
+			log.Sugar.Errorf("josn 序列化原始响应消息错误:%s", jerr.Error())
+			return nil
+		}
+
+		//保存上游信息
+		err = orm.Gdb.Model(model.Photo{}).Where("id = ?", newPhoto.ID).Update("out_message", string(originOutMessageBytes)).Error
+		if err != nil {
+			log.Sugar.Errorf("update data to photo has error:%s", err.Error())
+			return err
+		}
+	} else {
+		if c.Update().Message.Photo == nil {
+			return nil
+		}
+
+		var uniqueID string
+		if c.Message().ReplyTo.Photo != nil { //回复的是图片信息
+			uniqueID = c.Message().ReplyTo.Photo.UniqueID
+		} else if c.Message().ReplyTo.Video != nil {
+			uniqueID = c.Message().ReplyTo.Video.UniqueID
+		}
+
+		var dbPhoto model.Photo
+		err := orm.Gdb.Model(model.Photo{}).Where("unique_id = ?", uniqueID).First(&dbPhoto).Error
+		if err != nil {
+			log.Sugar.Errorf("sql query from photo has error:%s", err.Error())
+			return nil
+		}
+
+		// 图片是否来自下游群
+		if dbPhoto.ID > 0 {
+
+			//反序列化原始消息
+			originMessageStr := dbPhoto.OutMessage
+
+			var message tb.Message
+
+			err = json.Unmarshal([]byte(originMessageStr), &message)
 			if err != nil {
-				log.Sugar.Errorf("查询上游群图片信息失败:%s", err.Error())
+				log.Sugar.Errorf("反序列化原始消息错误:%s", err.Error())
 				return err
 			}
 
-			//转发到上游群
-			to := tb.Chat{ID: channel.ChannelId}
-
-			var outChanMsg tb.Message
-
-			jerr = json.Unmarshal([]byte(outChanPhoto.OutMessage), &outChanMsg)
-			if jerr != nil {
-				log.Sugar.Errorf("json反序列化上游群消息响应失败:%s", err.Error())
-			}
+			//转发到下游群
+			to := tb.Chat{ID: message.Chat.ID}
 
 			sendOpts := tb.SendOptions{}
-			sendOpts.ReplyTo = &outChanMsg
+			sendOpts.ReplyTo = &message
 
-			//_, err = c.Bot().Forward(&to, c.Message(), tb.ForceReply)
-			_, err = c.Bot().Send(&to, "这笔单子加急!麻烦尽快处理一下!", &sendOpts)
+			//回复给下游的消息
+			var replayMsg interface{}
+			//上游回复是否带图片
+			if c.Update().Message.Photo != nil {
+				photo := c.Message().Photo
+				photo.Caption = c.Text()
+				replayMsg = photo
+			} else if c.Update().Message.Video != nil {
+				video := c.Message().Video
+				video.Caption = c.Text()
+				replayMsg = video
+			} else {
+				replayMsg = c.Message().Text
+			}
+
+			_, err = c.Bot().Send(&to, replayMsg, &sendOpts)
 			if err != nil {
 				log.Sugar.Errorf("Forward error:%s", err.Error())
 				return err
 			}
 
-			return c.Reply("已加急~ 请勿重复发送视频哦")
 		}
-
-		log.Sugar.Errorf("insert data to photo has error:%s", err.Error())
-
-		return err
-	}
-
-	//开始转发操作
-	to := tb.Chat{ID: channel.ChannelId}
-
-	//photo := c.Message().Photo
-	//photo.Caption = c.Text()
-
-	video := c.Message().Video
-	video.Caption = c.Text()
-
-	outMessage, err := c.Bot().Send(&to, video) //tb.Protected
-	if err != nil {
-		log.Sugar.Errorf("Forward error:%s", err.Error())
-		return err
-	}
-
-	originOutMessageBytes, jerr := json.Marshal(*outMessage)
-	if jerr != nil {
-		log.Sugar.Errorf("josn 序列化原始响应消息错误:%s", jerr.Error())
-		return nil
-	}
-
-	//保存上游信息
-	err = orm.Gdb.Model(model.Photo{}).Where("id = ?", newPhoto.ID).Update("out_message", string(originOutMessageBytes)).Error
-	if err != nil {
-		log.Sugar.Errorf("update data to photo has error:%s", err.Error())
-		return err
 	}
 
 	return c.Reply("正在为你处理中... 请稍等~")
